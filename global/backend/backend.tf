9ae4564872b185a40dafca780a25211617f72773
mmd-afegbua <afegbuaismaila@gmail.com>
2020-09-14 11:47:19 +0100

    initialized backend with s3 and dynamoDB

--- global/backend/backend.tf
+++ global/backend/backend.tf
@@ -1,12 +1,10 @@
-/*
 terraform {
     backend "s3" {
-        bucket = "tusky"
+        bucket = "tusky-remote"
         key = "global/backend/terraform.tfstate"
         region = "us-east-1"
 
-        dynamodb_table = "poppy-carts-locks"
+        dynamodb_table = "tusky"
         encrypt = true
     }
 }
-*/
